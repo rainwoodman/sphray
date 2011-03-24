fd9df8a0afd244bbd8a98a6258e151167b15556f
Yu Feng <rainwoodman@gmail.com>
2011-03-24 11:41:50 -0400

    improve the good section of resolve to an O(1).
    added some time accounting routines.

--- mainloop.F90
+++ mainloop.F90
@@ -22,7 +22,8 @@ module mainloop_mod
   use global_mod, only: psys
   use global_mod, only: tree
   use global_mod, only: GV
-  use accounting_mod, only: AV, accounting_variables_type, reduce_accounting_variables, clear_accounting_variables
+  use accounting_mod, only: AV
+  use accounting_mod
   use global_mod, only: PLAN
   use global_mod, only: active_rays
   use config_mod, only: CV
@@ -166,7 +167,6 @@ contains
           ! done creation of a ray
           enddo
 
-         print *, "tracing"
          allocate(raylists(CV%IonFracOutRays))
          NTRD = OMP_GET_MAX_THREADS()
 
@@ -175,6 +175,7 @@ contains
            call clear_accounting_variables(localAVs(tid))
          enddo
 
+         call timer_init(AV, 1, "tracing", .True.)
          !$OMP PARALLEL FIRSTPRIVATE(rayn, TID, j, intersection)
 
          TID = OMP_GET_THREAD_NUM()
@@ -187,29 +188,40 @@ contains
           enddo
          !$OMP END DO
          !$OMP END PARALLEL
-         print *, "done tracing"
+         call timer_print(AV,1)
 
          ! this section resolves the races and causalities
-         call prepare_resolution(resolution, raylists)
+         call prepare_resolution(resolution, raylists, psys%par)
 
+         call timer_init(AV, 2, 'resolve', running=.False.)
          do while(resolution%remaining_nnb > 0)
+         call timer_init(AV, 3, 'good', running=.False.)
+         call timer_init(AV, 4, 'bad', running=.False.)
+         call timer_init(AV, 5, 'update', running=.False.)
            if (NTRD == 1) then
              call resolve_all(resolution, raylists)
            else
              call resolve_more(resolution, raylists)
            endif
            ! this section updates the intersections
-           !$OMP PARALLEL FIRSTPRIVATE(j, TID, intersection) IF(resolution%good_nnb > 10)
+           call timer_resume(AV,5)
+           !!$OMP PARALLEL FIRSTPRIVATE(j, TID, intersection) IF(resolution%good_nnb > 10)
            TID = OMP_GET_THREAD_NUM()
-           !$OMP DO 
+           !!$OMP DO 
            do j = 1, resolution%good_nnb
              call resolution_get_resolved_intersection(resolution, raylists, j, intersection)
-!             print *, 'inte', intersection%pindx, intersection%rayn, intersection%t
+             print *, 'inte', intersection%pindx, intersection%rayn, intersection%t
 !             print *, active_rays(intersection%rayn)%emit_time, psys%par(intersection%pindx)%lasthit
              call update_intersection(intersection, psys%par,psys%box, localAVs(TID))
            enddo 
-           !$OMP END DO
-           !$OMP END PARALLEL
+           !!$OMP END DO
+           !!$OMP END PARALLEL
+           call timer_pause(AV,5)
+         print *, 'goodnnb', resolution%good_nnb
+         call timer_print(AV, 2)
+         call timer_print(AV, 3)
+         call timer_print(AV, 4)
+         call timer_print(AV, 5)
          end do
 
          ! free up the memory from the globalraylist.
--- resolve.F90
+++ resolve.F90
@@ -8,6 +8,8 @@ module resolve_mod
   use myf03_mod
   use m_mrgrnk, only: mrgrnk
   use raylist_mod, only: intersection_type, raylist_type
+  use particle_system_mod, only: particle_type
+  use accounting_mod
   implicit none
   type resolution_type
     integer(i8b):: remaining_nnb  !< nnb that remains in encoded_impacts
@@ -15,6 +17,8 @@ module resolve_mod
     integer(i8b), allocatable:: encoded(:)
     integer(i8b), allocatable:: plink(:)
     integer(i8b),allocatable :: good(:)
+    integer(i8b),allocatable :: good_pindx(:)
+    logical(i1b), allocatable :: par_in_good(:)
     integer(i8b):: good_nnb
     integer(i8b),allocatable :: pool_head(:)
     integer(i8b),allocatable :: pool_cur(:)
@@ -53,12 +57,17 @@ contains
      r = a%t > b%t .and. race(a,b)
   end function cconflict
 
-  subroutine prepare_resolution(resolution, raylists)
+  subroutine prepare_resolution(resolution, raylists, par)
      type(raylist_type), intent(in), allocatable :: raylists(:)
+     type(particle_type), intent(in), allocatable :: par(:)
      type(resolution_type), intent(inout) :: resolution
      integer(i8b),allocatable :: pindx(:), indexx(:)
      integer(i8b) :: total_nnb,  j, this, last, first, impact
      integer(i4b) :: rayln
+     allocate(resolution%par_in_good(size(par, 1)))
+     do j = 1, size(par, 1), 1
+        resolution%par_in_good(j) = .False.
+     enddo
      allocate(resolution%pool_head(size(raylists, 1)))
      allocate(resolution%pool_cur(size(raylists, 1)))
      allocate(resolution%pool_tail(size(raylists, 1)))
@@ -76,7 +85,8 @@ contains
      resolution%pool_cur = resolution%pool_head
 
      resolution%secret = size(raylists, 1) + 1
-     allocate(resolution%good(total_nnb))
+     allocate(resolution%good(10000))
+     allocate(resolution%good_pindx(10000))
      allocate(resolution%encoded(total_nnb))
      allocate(resolution%plink(total_nnb))
      ! create the link list of intersections with same particles
@@ -94,9 +104,9 @@ contains
      call mrgrnk(pindx, indexx)
 
      print *, 'sorted'
-     first = indexx(1)
-     last =indexx(1)
-     do j = 2, total_nnb, 1
+     first = indexx(total_nnb)
+     last =indexx(total_nnb)
+     do j = total_nnb - 1, 1, -1
        this = indexx(j)
        if (pindx(this) /= pindx(last)) then
          resolution%plink(last) = first
@@ -158,6 +168,9 @@ contains
      logical(i4b) :: good_candidate
      integer(i4b) :: rayln, raylm
      integer(i8b) :: j, k, jmpact, this, first
+     integer(i8b) :: counter
+     integer(i8b) :: c_pindx
+     call timer_resume(AV, 2)
      good_tail = 0
      !do rayln = 1, size(raylists, 1)
      !  print *, resolution%pool_head(rayln), resolution%pool_cur(rayln), resolution%pool_tail(rayln)
@@ -166,26 +179,28 @@ contains
         if (resolution%pool_cur(rayln) > resolution%pool_tail(rayln)) then 
             cycle
         endif
-        c = raylists(rayln)%intersections(resolution%pool_cur(rayln) - resolution%pool_head(rayln) + 1)
+        c_pindx = raylists(rayln)%intersections(resolution%pool_cur(rayln) - resolution%pool_head(rayln) + 1)%pindx
         good_candidate = .True.
-        do j = 1, good_tail, 1
-           call resolution_get_resolved_intersection(resolution, raylists, j, a)
-           if (a%pindx  == c%pindx ) then 
+        call timer_resume(AV, 3)
+        if (resolution%par_in_good(c_pindx)) then
               good_candidate = .False.
-              exit
-           endif
-        enddo
+        endif
+        call timer_pause(AV, 3)
+        call timer_resume(AV, 4)
         if (good_candidate) then
+           counter = 0
            !print *, 'candidate', c%pindx, c%rayn, c%t
            first = resolution%pool_cur(rayln)
            this = resolution%plink(first)
            do while (this /= first)
               if(this > first) then
-                this = resolution%plink(this)
-                cycle
+                 exit ! because the link list is reverse sorted
+                      ! if 'this' > 'first', first is already the
+                      ! earliest impact on the particle
+                      ! we can move it wherever we want
               endif
               call decode(resolution, resolution%encoded(this), raylm, jmpact)
-              a = raylists(raylm)%intersections(jmpact)
+              !a = raylists(raylm)%intersections(jmpact)
               !print *, '  compare', a%pindx, a%rayn, a%t
               if(raylm >= rayln) then
                  stop "can't happen. an earlier intersection must be in an earlier ray"
@@ -197,19 +212,28 @@ contains
                  exit
               endif
               this = resolution%plink(this)
+              counter = counter + 1
            enddo
         endif
+        call timer_pause(AV, 4)
         if (good_candidate) then
           good_tail = good_tail + 1
-          resolution%good(good_tail) = encode(resolution, rayln, resolution%pool_cur(rayln) - resolution%pool_head(rayln) + 1)
+          resolution%good(good_tail) = resolution%pool_cur(rayln)
+          resolution%par_in_good(c_pindx) = .True.
+          resolution%good_pindx(good_tail) = c_pindx
           resolution%pool_cur(rayln) = resolution%pool_cur(rayln) + 1
-        else
+        endif
+        if (good_tail == 10000) then 
           exit
         endif
      enddo
 
+     do j = 1, good_tail, 1
+       resolution%par_in_good(resolution%good_pindx(j)) = .False.
+     enddo
      resolution%good_nnb = good_tail
      resolution%remaining_nnb = resolution%remaining_nnb - good_tail
+     call timer_pause(AV, 2)
   end subroutine resolve_more
 
   subroutine resolution_get_resolved_intersection(resolution, raylists, j, intersection)
@@ -219,17 +243,19 @@ contains
      integer(i8b), intent(in):: j
      integer(i8b):: impact
      integer(i4b):: rayln
-     call decode(resolution, resolution%good(j), rayln, impact)
+     call decode(resolution, resolution%encoded(resolution%good(j)), rayln, impact)
      intersection = raylists(rayln)%intersections(impact)
   endsubroutine resolution_get_resolved_intersection
   subroutine kill_resolution(resolution)
     type(resolution_type), intent(inout):: resolution
     deallocate(resolution%good)
+    deallocate(resolution%good_pindx)
     deallocate(resolution%plink)
     deallocate(resolution%encoded)
     deallocate(resolution%pool_head)
     deallocate(resolution%pool_cur)
     deallocate(resolution%pool_tail)
+    deallocate(resolution%par_in_good)
     resolution%secret = 0
   endsubroutine kill_resolution
 end module
